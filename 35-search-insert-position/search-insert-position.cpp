2591cbdba535f3f6fb0a6ce3b547ba08256f1816
OXFury <mnvsaxena07@gmail.com>
2026-07-16 10:42:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.25%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(target>nums[nums.size()-1])
+        {
+            return nums.size();
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            
+            if(target==nums[i])
+            {
+                return i;
+                
+            }
+            if(nums[i]>target)
+            {
+                return i;
+            }
+             
+            
+        }
+        return 0;
+    }
+};
